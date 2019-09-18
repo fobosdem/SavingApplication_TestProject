9393ca7fccaf98659ea370bb9f7b13a46228b522
fobosdem <fobosdem@gmail.com>
2019-09-18 13:56:45 +0300

    Updating Models

---
+++ SavingApplication_TestProject/WebApplication/Models/Messages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+	public class Messages
+	{
+		public string MessageType { get; set; }
+		public string Message { get; set; }
+	}
+}
---
+++ SavingApplication_TestProject/WebApplication/Models/Users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+	public class Users
+	{
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string NickName { get; set; }
+		public string Password { get; set; }
+	}
+}
