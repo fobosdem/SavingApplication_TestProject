b6f775d63bf8c38ff8875a6d613d48532af10b28
fobosdem <fobosdem@gmail.com>
2019-09-18 14:51:51 +0300

    Edding class for working with DB

---
+++ SavingApplication_TestProject/WebApplication/Models/DBconncet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+	public class DBconncet : DbContext
+	{
+		public DBconncet() : base("SavingHistoryDB")
+		{ }
+		public DbSet<Users> Users { get; set; }
+		public DbSet<Chats> Chats { get; set; }
+		public DbSet<Messages> Messages { get; set; }
+	}
+}
